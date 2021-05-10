7e3747c74d1d6bcdec1572cee648c4bb58b4821f
dashamoolam <77241632+dashamoolam@users.noreply.github.com>
2021-05-11 02:04:30 +0530

    Update sample_secrets.py

--- sample_secrets.py
+++ sample_secrets.py
@@ -1,6 +1,9 @@
+import os
+
+
 # this is the bot token, gotten from BotFather
-TELEGRAM_BOT_TOKEN = "update it here"
+TELEGRAM_BOT_TOKEN = os.environ.get("TOKEN")
 # this is your own telegram ID, which is used to send exception tracebacks
-DEVELOPER_CHAT_ID = ""
+DEVELOPER_CHAT_ID = os.environ.get("TOKEN")
 # list of telegram IDs for them /stats command is enabled
 MAINTAINERS_CHAT_IDS = [DEVELOPER_CHAT_ID, ]
