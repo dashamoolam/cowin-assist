746f1bc289b338437ab15bf525254a5ee7163ef3
dashamoolam <77241632+dashamoolam@users.noreply.github.com>
2021-05-11 02:09:54 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -4,6 +4,6 @@ import os
 # this is the bot token, gotten from BotFather
 TELEGRAM_BOT_TOKEN = os.environ.get("TOKEN")
 # this is your own telegram ID, which is used to send exception tracebacks
-DEVELOPER_CHAT_ID = os.environ.get("TOKEN")
+DEVELOPER_CHAT_ID = os.environ.get("SUDO")
 # list of telegram IDs for them /stats command is enabled
 MAINTAINERS_CHAT_IDS = [DEVELOPER_CHAT_ID, ]
