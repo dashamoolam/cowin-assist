3fcac8f8ee9cec93a14d76b8e944404088c905e4
dashamoolam <77241632+dashamoolam@users.noreply.github.com>
2021-05-13 12:59:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -363,7 +363,8 @@ def check_slots_command(update: Update, ctx: CallbackContext) -> None:
     vaccination_centers: List[VaccinationCenter]
     try:
         vaccination_centers = get_available_centers_by_pin(user.pincode)
-    except CoWinTooManyRequests:
+    except:
+        print(error)
         update.effective_chat.send_message(
             F"Hey sorry, I wasn't able to reach [CoWin Site](https://www.cowin.gov.in/home) at this moment. "
             "Please try after few minutes.", parse_mode="markdown")
